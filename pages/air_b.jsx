7df3fd261c9fb919cd61edbfc0be1c0c6e3203c6
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-05-24 18:58:29 +0530

    Added Loading

--- pages/air_b.jsx
+++ pages/air_b.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
-
-import Head from "next/head";
-import Image from "next/image";
+import { useState, useEffect } from "react";
 
 import Header from "../components/Header";
 import CardGrid from "../components/CardGrid";
@@ -9,16 +6,21 @@ import CardGrid from "../components/CardGrid";
 import styles from "../styles/Home.module.scss";
 
 export default function Home({ initialData }) {
+    const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState(initialData);
 
     const fetchData = async () => {
-        const req = await fetch("http://127.0.0.1:5000/fetch/air_b");
-        console.log(req)
-        const newData = await req.json();
-        setData(newData);
-    };
+        setIsLoading(true);
+        const res = await fetch("http://127.0.0.1:5000/fetch/AIR_b")
+            .then((res) => res.json())
+            .catch((rejected) => {
+                console.log(rejected);
+            });
 
-    const handleClick = (event) => {
+        setData(res);
+        setIsLoading(false);
+    };
+    const handleClick = async () => {
         event.preventDefault();
         fetchData();
     };
@@ -27,7 +29,9 @@ export default function Home({ initialData }) {
             <Header />
             <div className={styles.container}>
                 <div className={styles.head}>
-                    <button onClick={handleClick}>{data.last_updated}</button>
+                    <button onClick={handleClick}>
+                        {isLoading ? "Loading ..." : data.last_updated}
+                    </button>
                 </div>
                 <main className={styles.main}>
                     <CardGrid details={data.data.assignments} />
@@ -37,17 +41,9 @@ export default function Home({ initialData }) {
     );
 }
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries. See the "Technical details" section.
-export async function getServerSideProps() {
-    // Call an external API endpoint to get posts.
-    // You can use any data fetching library
-    const res = await fetch("http://127.0.0.1:5000/air_b");
+export async function getStaticProps() {
+    const res = await fetch("http://127.0.0.1:5000/AIR_b");
     const initialData = await res.json();
-
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
     return {
         props: {
             initialData,
