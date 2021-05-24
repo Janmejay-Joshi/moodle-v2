4a0469417aef4bc1f2d20d360fa183acf4ab1a0e
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-05-25 01:11:31 +0530

    Testin

---
+++ api/cached.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        branch = branch.upper()
+        with open(f'./data/data_{branch}.json', mode='r') as my_file:
+            text = loads(my_file.read())
+
+        self.wfile.write(text)
+        return
