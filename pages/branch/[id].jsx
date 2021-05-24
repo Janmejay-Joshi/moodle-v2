851b5e943b44fc4346f71ed0f62578aedc69397e
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-05-24 19:56:36 +0530

    Added Dynamic Routing

---
+++ pages/branch/[id].jsx
@@ -0,0 +1,68 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+import Header from "../../components/Header";
+import CardGrid from "../../components/CardGrid";
+
+import styles from "../../styles/Home.module.scss";
+
+const Post = ({ initialData }) => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState(initialData);
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        const res = await fetch(`http://127.0.0.1:5000/fetch/${id}`)
+            .then((res) => res.json())
+            .catch((rejected) => {
+                console.log(rejected);
+            });
+
+        setData(res);
+        setIsLoading(false);
+    };
+    const handleClick = async () => {
+        event.preventDefault();
+        fetchData();
+    };
+    return (
+        <>
+            <Header />
+            <div className={styles.container}>
+                <div className={styles.head}>
+                    <button onClick={handleClick}>
+                        {isLoading ? "Loading ..." : data.last_updated}
+                    </button>
+                </div>
+                <main className={styles.main}>
+                    <CardGrid details={data.data.assignments} />
+                </main>
+            </div>
+        </>
+    );
+};
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { id: "air_a" } },
+            { params: { id: "air_b" } },
+            { params: { id: "ece" } },
+        ],
+        fallback: false, // See the "fallback" section below
+    };
+}
+
+export async function getStaticProps({params}) {
+    const res = await fetch(`http://127.0.0.1:5000/cached/${params.id}`);
+    const initialData = await res.json();
+    return {
+        props: {
+            initialData,
+        },
+    };
+}
+export default Post;
