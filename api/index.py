f60b24e75727896913a1e88722ec769562510143
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-05-24 20:03:07 +0530

    changes for vercel

--- api/index.py
+++ api/index.py
@@ -8,13 +8,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    """TODO: Docstring for index.
-    :returns: TODO
-
-    """
-    return "Instead Try /cached/air_a or /cached/air_b" 
+@app.route('/', defaults={'path': ''})
 
 @app.route('/cached/<branch>')
 def cached(branch):
