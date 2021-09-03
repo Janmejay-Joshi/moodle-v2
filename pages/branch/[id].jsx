70b84ca8c743f1618438716a054118e8a1ec862f
Janmejay Joshi <Janmejay-Joshi>
2021-09-03 10:41:04 +0530

    Reduced Bloat

--- pages/branch/[id].jsx
+++ pages/branch/[id].jsx
@@ -1,66 +1,43 @@
-//import { useRouter } from 'next/router';
-//import { useState } from 'react';
-
 import Header from '../../components/Header';
 import CardGrid from '../../components/CardGrid';
 
 import styles from '../../styles/Home.module.scss';
 
-const Post = ({ initialData }) => {
-    const data = initialData;
+const Post = ({initialData}) => {
+  const data = initialData;
+  const today = new Date();
 
-    /*
-    const router = useRouter();
-    const { id } = router.query;
+  const old = new Date(data.last_updated);
+  let difference = Math.round((today.getTime() - old.getTime()) / (1000 * 60));
+  difference =
+    difference > 60
+      ? `${Math.round(difference / 60)} Hours ${difference % 60} minutes`
+      : `${difference} minutes`;
 
-    //    const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState(initialData);
-    const fetchData = async () => {
-        setIsLoading(true);
-        const res = await fetch(`https://7qoju2.deta.dev/fetch/${id}`)
-            .then((res) => res.json())
-            .catch((rejected) => {
-                console.log(rejected);
-            });
-
-        setData(res);
-        setIsLoading(false);
-    };
-    const handleClick = async () => {
-        fetchData();
-    };
-    */
-
-    const today = new Date();
-    const old = new Date(data.last_updated);
-    let difference = Math.round((today.getTime() - old.getTime()) / (1000 * 60));
-    difference =
-        difference > 60 ? `${Math.round(difference / 60)} Hours ${difference % 60} minutes` : `${difference} minutes`;
-
-    return (
-        <>
-            <Header />
-            <div className={styles.container}>
-                <div className={styles.head}>
-                    <div className={styles.update}>
-                        Last Fetched Assignments <span>{difference}</span> ago.
-                    </div>
-                </div>
-                <main className={styles.main}>
-                    <CardGrid details={data.data.assignments} />
-                </main>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.head}>
+          <div className={styles.update}>
+            Last Fetched Assignments <span>{difference}</span> ago.
+          </div>
+        </div>
+        <main className={styles.main}>
+          <CardGrid details={data.data.assignments} />
+        </main>
+      </div>
+    </>
+  );
 };
 
-export async function getServerSideProps({ params }) {
-    const res = await fetch(`https://7qoju2.deta.dev/cached/${params.id}`);
-    const initialData = await res.json();
-    return {
-        props: {
-            initialData,
-        },
-    };
+export async function getServerSideProps({params}) {
+  const res = await fetch(`https://7qoju2.deta.dev/cached/${params.id}`);
+  const initialData = await res.json();
+  return {
+    props: {
+      initialData,
+    },
+  };
 }
 export default Post;
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,31 @@
-import { useState } from "react";
-import Link from "next/link";
+import Link from 'next/link';
+import Header from '../components/Header';
 
-import Head from "next/head";
-import Image from "next/image";
-
-import Header from "../components/Header";
-import CardGrid from "../components/CardGrid";
-
-import styles from "../styles/Home.module.scss";
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
-    return (
-        <>
-            <Header />
-            <div className={styles.container}>
-                <div className={styles.main}>
-                    <Link href="/branch/air_a">
-                        <div className={styles.branch}>
-                            <a>AIR Group A</a>
-                        </div>
-                    </Link>
-                    <Link href="/branch/air_b">
-                        <div className={styles.branch}>
-                            <a>AIR Group B</a>
-                        </div>
-                    </Link>
-                    <Link href="/branch/ece">
-                        <div className={styles.branch}>
-                            <a>ECE</a>
-                        </div>
-                    </Link>
-                </div>
+  return (
+    <>
+      <Header />
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <Link href="/branch/air_a">
+            <div className={styles.branch}>
+              <a>AIR Group A</a>
             </div>
-        </>
-    );
+          </Link>
+          <Link href="/branch/air_b">
+            <div className={styles.branch}>
+              <a>AIR Group B</a>
+            </div>
+          </Link>
+          <Link href="/branch/ece">
+            <div className={styles.branch}>
+              <a>ECE</a>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
