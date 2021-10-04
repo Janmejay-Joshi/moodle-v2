0b16027301a559095b7ceba8f74ddadc3622580c
Janmejay-Joshi <publish.janmejay@gmail.com>
2021-10-04 10:09:27 +0530

    Fixed Timing Issue

--- components/CardGrid/Card/Card.jsx
+++ components/CardGrid/Card/Card.jsx
@@ -4,35 +4,39 @@ const Card = (props) => {
   const today = new Date();
   const old = new Date(props.due_date);
 
-  let difference = Math.round((today.getTime() - old.getTime()) / (1000 * 60));
+  let difference = Math.round((old.getTime() - today.getTime()) / (1000 * 60));
   difference =
-    difference > 60
-      ? `${Math.round(difference / 60)} Hours ${difference % 60} minutes`
+    Math.abs(difference) > 60 * 24
+      ? ` ${Math.round(difference / (60 * 24)) - 1} Days ${Math.abs(
+          Math.round((difference / 60) % 24) - 1
+        )} Hours ${Math.abs(difference % 60)} minutes`
+      : Math.abs(difference) > 60
+      ? `${Math.round(difference / 60)} Hours ${Math.abs(
+          difference % 60
+        )} minutes`
       : `${difference} minutes`;
 
-    return (
-        <a href={props.link} target="_blank" rel="noopener noreferrer">
-            <div
-                className={styles.card}
-                style={
-                    props.due
-                        ? { border: "solid thick red" }
-                        : { border: "solid thick green" }
-                }
-            >
-                <div className={styles.card_head}>
-                    {props.subject.toUpperCase()}
-                </div>
-                <div className={styles.card_body}>
-                    <ul>
-                        <li>{props.title.toUpperCase()}</li>
-                        <li>{props.due}</li>
-                        <li>{difference}</li>
-                    </ul>
-                </div>
-            </div>
-        </a>
-    );
+  return (
+    <a href={props.link} target="_blank" rel="noopener noreferrer">
+      <div
+        className={styles.card}
+        style={
+          props.due
+            ? { border: "solid thick red" }
+            : { border: "solid thick green" }
+        }
+      >
+        <div className={styles.card_head}>{props.subject.toUpperCase()}</div>
+        <div className={styles.card_body}>
+          <ul>
+            <li>{props.title.toUpperCase()}</li>
+            <li>{props.due}</li>
+            <li>{difference}</li>
+          </ul>
+        </div>
+      </div>
+    </a>
+  );
 };
 
 export default Card;
